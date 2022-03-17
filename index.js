56fd6eaf45bb66a55e777e647086a8e1ac5426db
tsunodajapa <gabrieltsunoda@gmail.com>
2022-03-16 21:07:20 -0300

    feat: add express validator

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path');
+const { body, validationResult } = require('express-validator');
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 function testantoMiddleware(request, response, next) {
     console.log('Entrei na middleware');
@@ -12,12 +16,12 @@ function testantoMiddleware(request, response, next) {
 function podeEntrarNaRota(request, response, next) {
     console.log('Entrei na middleware 2');
     console.log(request.query.nome);
+    next();
+    // if (request.query.nome === 'yuki') {
+    //     return next();
+    // }
 
-    if (request.query.nome === 'yuki') {
-        return next();
-    }
-
-    return response.status(401).send('Você não tem permissão');
+    // return response.status(401).send('Você não tem permissão');
 }
 
 function imprimindoParametro(request, response, next) {
@@ -25,6 +29,10 @@ function imprimindoParametro(request, response, next) {
 
     next();
 }
+
+const validarLogin = [
+    body('login').notEmpty().withMessage('Deve preencher o login')
+]
 
 // Definindo ejs como template e pastatas da views
 app.set('views', path.join(__dirname, 'src', 'views'));
@@ -36,7 +44,15 @@ app.use(podeEntrarNaRota);
 
 // [GET]/ animais
 
-app.get('/', (request, response) => {
+app.get('/login', (request, response) => {
+    response.render('index');
+});
+
+app.post('/login', validarLogin, (request, response) => {
+    const erros = validationResult(request);
+
+    console.log(erros);
+
     response.render('index');
 });
 
